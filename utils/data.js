436efa8fcadab0802797e4f07e511e5ca057e16c
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-04 23:06:54 -0600

    done?

---
+++ utils/data.js
@@ -0,0 +1,52 @@
+
+const users = [
+    {
+        username: 'Alpha',
+        email: 'Alpha1@gmail.com'
+    },
+    {
+        username: 'Beta',
+        email: 'Beta1@gmail.com'
+    },
+    {
+        username: 'Delta',
+        email: 'Delta1@gmail.com'
+    },
+    {
+        username: 'Omega',
+        email: 'Omega1@gmail.com'
+    },
+    {
+        username: 'Sigma',
+        email: 'Sigma1@gmail.com'
+    }
+];
+
+const thoughts = [
+    {
+        text: "I'm Alpha",
+        username: 'Alpha',
+        reacts:{
+            reactBody: 'Weak',
+            username: 'Sigma'
+        }
+    },
+    {
+        text: "Alpha is so cool",
+        username: 'Beta',
+        reacts:{
+            reactBody: 'Simp',
+            username: 'Omega'
+        }
+    },
+    {
+        text: "I'm cool too!",
+        username: 'Delta',
+        reacts:{
+            reactBody: 'or not',
+            username: 'Delta'
+        }
+    }
+];
+
+module.exports = {users, thoughts}
---
+++ utils/seed.js
@@ -0,0 +1,32 @@
+const connection = require('../config/connection');
+const { User, Thought, React } = require('../models');
+const { users, thoughts } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+
+  let reactionCheck = await connection.db.listCollections({ name: 'reactions' }).toArray();
+  if (reactionCheck.length) {
+    await connection.dropCollection('reactions');
+  }
+
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  console.table(users);
+  console.table(thoughts);
+  console.info('Seeding successfuly completed')
+  process.exit(0);
+});
