34074915c037f8742ff15590a194f8ec6be2e3a3
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-04 20:59:56 -0600

    a lot of work

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: isEmail, message:'Not A Email'
+            }
+        },
+        thoughs: [{type: Schema.Types.ObjectId, ref: 'Thought'}],
+        friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model ('user', userSchema);
+
+module.exports = User;
